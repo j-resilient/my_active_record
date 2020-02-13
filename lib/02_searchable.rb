ec6d509f031105c255646123f33b2d0c07bf3905
J Resilient <j.resilient.1@gmail.com>
2020-02-13 15:50:41 -0500

    Add Searchable module to add #where

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,18 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map { |k, v| "#{k.to_s} = ?"}.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, params.values)
+      SELECT *
+      FROM #{self.table_name}
+      WHERE #{where_line}
+    SQL
+
+    results.map { |r| self.new(r) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
