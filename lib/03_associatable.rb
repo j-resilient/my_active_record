d64ae59e4f37ca17443190ef719f4273c612fa30
J Resilient <j.resilient.1@gmail.com>
2020-11-25 18:18:11 -0500

    Add and comment Associatable#belongs_to

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -9,10 +9,12 @@ class AssocOptions
     :primary_key
   )
 
+  # returns this object's class
   def model_class
     self.class_name.constantize
   end
 
+  # returns this objects table
   def table_name
     model_class.table_name
   end
@@ -20,9 +22,9 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-  @foreign_key = options.has_key?(:foreign_key) ? options[:foreign_key] : "#{name}_id".to_sym
-  @primary_key = options.has_key?(:primary_key) ? options[:primary_key] : :id
-  @class_name = options.has_key?(:class_name) ? options[:class_name] : "#{name.to_s.capitalize}"
+    @foreign_key = options.has_key?(:foreign_key) ? options[:foreign_key] : "#{name}_id".to_sym
+    @primary_key = options.has_key?(:primary_key) ? options[:primary_key] : :id
+    @class_name = options.has_key?(:class_name) ? options[:class_name] : "#{name.to_s.capitalize}"
   end
 end
 
@@ -36,16 +38,33 @@ end
 
 module Associatable
   # Phase IIIb
+  # this is the method I got stuck on:
+  # I reviewed the solution code and didn't get it,
+  # copied it over and made it work and still didn't get it,
+  # came back to it later and still didn't give it
+  # and I have decided that stopping here is better than crying indefinitely over code
   def belongs_to(name, options = {})
-    assoc_options = BelongsToOptions.new(name, options)
+    # saves the association name and options in a BelongsToOptions obj
+    options = BelongsToOptions.new(name, options)
 
+    # create the actual association method
+    # belongs_to :cat,
+    #   primary_key: :id,
+    #   foreign_key: :cat_id,
+    #   class_name: "Cat"
+    # primary_key -> the current object's id in its own table
+    # foreign_key -> the cat's id in the current' object's table
+    # class_name  -> the name of the other class in the association
     define_method(name) do
-      options = assoc_options
-
-      key_val = self.send(options.foreign_key)
+      # get the foreign_key value
+      for_val = self.send(options.foreign_key)
+      # get the class_name from options#model_class
+      # call #where on that class, looking for a row where the primary key is the desired cat
+      # get the first record: that's the cat we want
+      # return it
       options
         .model_class
-        .where(options.primary_key => key_val)
+        .where(options.primary_key => for_val)
         .first
     end
   end
