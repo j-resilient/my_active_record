c5ed794180302686958dd9ebb58842653b7032e0
J Resilient <j.resilient.1@gmail.com>
2020-12-01 18:41:23 -0500

    Add #has_one_through to Associatable

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,10 +1,30 @@
 require_relative '03_associatable'
+require_relative 'db_connection'
 
 # Phase IV
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      source_table = source_options.table_name
+      through_table = through_options.table_name
+      foreign_key_name = source_options.class_name.downcase + "_id"
+
+      id = self.send(through_options.foreign_key)
+
+      result = DBConnection.execute(<<-SQL, id)
+        SELECT #{source_table}.*
+        FROM #{through_table}
+        JOIN #{source_table} ON #{through_table}.#{foreign_key_name} = #{source_table}.id
+        WHERE #{through_table}.id = ?
+        LIMIT 1
+      SQL
+
+      source_options.model_class.parse_all(result).first
+    end
   end
 end
